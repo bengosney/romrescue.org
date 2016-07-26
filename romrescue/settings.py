c70417a3ab03dd45d6f8fa4581daf9ad2da16b15
Ben Gosney <bengosney@googlemail.com>
2016-07-26 21:44:56 +0100

    static file serving

--- romrescue/settings.py
+++ romrescue/settings.py
@@ -19,6 +19,11 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
