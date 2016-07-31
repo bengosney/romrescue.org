170f07cc3fe005ea68acf3943dc481545330690f
Ben Gosney <bengosney@googlemail.com>
2016-07-31 22:51:43 +0100

    basic decorator to register listable views

--- pages/decorators.py
+++ pages/decorators.py
@@ -1,7 +1,16 @@
 
 form_registry = {}
-
+list_view_registry = []
 
 def register_form(cls):
     form_registry[cls.__name__] = cls()
     return cls
+
+def register_list_view(Cls):
+    list_view_registry.append((Cls.__name__, Cls.__name__))
+            
+    return Cls
+
+
+def get_registered_list_views():
+    return list_view_registry
