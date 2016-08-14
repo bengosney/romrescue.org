0e088d35aaa09527033241be50f7ce8677f84f8d
Ben Gosney <bengosney@googlemail.com>
2016-08-14 20:18:55 +0100

    added the status to the list page

--- modulestatus/admin.py
+++ modulestatus/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
+from . import ModelStatus
+
+
 class statusAdmin(object):
+    def __init__(self, model, admin_site):
+        super(statusAdmin, self).__init__(model, admin_site)
+
+        self.list_display = ['status'] + list(self.list_display)
+        self.list_filter = ['status'] + list(self.list_filter)
+
+
     def get_queryset(self, request):
         qs = self.model.admin_objects.get_queryset()
 
