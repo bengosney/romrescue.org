5dfddd50811a1ddd2602b1dcf5da0c40c82f8f06
Ben Gosney <bengosney@googlemail.com>
2017-07-31 22:44:44 +0100

    Removed unused and broken urls

--- dogs/urls.py
+++ dogs/urls.py
@@ -4,15 +4,9 @@ from . import views
 
 app_name = 'dogs'
 urlpatterns = [
-    url(r'^$',
-        views.AdoptionList.as_view(),
-        name='AdoptionList'),
     url(r'^success/(?P<slug>[\w-]+)/$',
         views.SuccessDogDetail.as_view(),
         name='SuccessDetail'),
-    url(r'^success/$',
-        views.SuccessList.as_view(),
-        name='SuccessList'),
     url(r'^(?P<slug>[\w-]+)/$',
         views.DogDetail.as_view(),
         name='DogDetails'),
