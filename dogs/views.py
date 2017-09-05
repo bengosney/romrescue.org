c031585817a847186687168c6b9e75ca1459b9c4
Ben Gosney <bengosney@googlemail.com>
2017-09-05 23:20:01 +0100

    fixes #53 Update the fn's if there is a duplicate found

--- dogs/views.py
+++ dogs/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import MultipleObjectsReturned
 from vanilla import DetailView, ListView
 from .models import Dog
 from pages.decorators import register_list_view
@@ -26,6 +27,18 @@ class DogDetail(DetailView):
     template_name = 'dogs/details.html'
     lookup_field = 'slug'
 
+    def get_object(self):
+        try:
+            return super(DogDetail, self).get_object()
+        except MultipleObjectsReturned:
+            lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+            lookup = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+            objects = self.get_queryset().filter(**lookup)
+            Dog._meta.get_field('slug').overwrite = True
+            for obj in objects[1:]:
+                obj.save()
+
+            return objects[0]
 
     def get_queryset(self):
         return self.model._default_manager.filter(dogStatus=Dog.STATUS_LOOKING)
