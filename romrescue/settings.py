6e48f78e88e71dcede9125bb38f58a0c143430e4
Ben Gosney <bengosney@googlemail.com>
2019-01-17 22:07:43 +0000

    Remove query string auth as the buckets are public and apprently not having valid auth stops public photos from showing

--- romrescue/settings.py
+++ romrescue/settings.py
@@ -143,6 +143,7 @@ THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'romrescue'
+AWS_QUERYSTRING_AUTH = False
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
