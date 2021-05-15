c4c30c5c4bfdeb7fc9fea437dda036d0c72fa913
Ben Gosney <bengosney@googlemail.com>
2021-05-15 13:50:46 +0100

    Remove debug code

--- donate/views.py
+++ donate/views.py
@@ -1,9 +1,6 @@
 # Django
 from django.shortcuts import render
 
-# Third Party
-from icecream import ic
-
 # First Party
 from donate.models import DontateSettings
 
@@ -11,7 +8,6 @@ from donate.models import DontateSettings
 def donate(request):
     settings = DontateSettings.get_solo()
 
-    ic(settings.values_set)
     context = {
         "donate": settings,
         "values": settings.values_set.all(),
