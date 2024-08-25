7a63315a69a427eb4fd94dbc4a60869086bad7e4
Ben Gosney <bengosney@googlemail.com>
2024-08-25 14:54:35 +0100

    fix(polymorphic): use a forked version of polymorphic
    This has the fix commited in rather than monkey patched

--- pages/apps.py
+++ pages/apps.py
@@ -1,24 +1,6 @@
 # Django
 from django.apps import AppConfig
 
-# Third Party
-import wrapt
-
-
-def test___lookup_wrapper(wrapped, instance, args, kwargs):
-    try:
-        return wrapped(*args, **kwargs)
-    except Exception:
-        pass
-
 
 class PagesConfig(AppConfig):
     name = "pages"
-
-    def ready(self) -> None:
-        wrapt.patch_function_wrapper(
-            "polymorphic.query",
-            "PolymorphicQuerySet._process_aggregate_args.test___lookup",
-            test___lookup_wrapper,
-        )
-        return super().ready()
